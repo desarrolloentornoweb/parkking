74cdf1ba62621055d502c4c9ce459893a0a2eb52
mvilladeza <U201722537@upc.edu.pe>
2019-02-06 14:16:00 -0500

    validaciones autenticacion

--- app/models/autenticacion.rb
+++ app/models/autenticacion.rb
@@ -1,3 +1,10 @@
 class Autenticacion < ApplicationRecord
   belongs_to :tipo_usuario
+
+    validates :correo, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+uniqueness: { message: "El correo ya existe", case_sensitive: false }
+
+validates :contrasena, presence: {message: " Ingresar una contraseña"}
+
+validates :tipo_usuario, presence: {message: " Seleccionar un tipo de usuario "}
 end
